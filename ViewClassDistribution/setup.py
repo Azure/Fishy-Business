e4c20f7daf3758bb6b9b883febe9b15497efad65
Steve VB <stvanbod@microsoft.com>
2018-07-22 14:37:56 +0930

    Added the View Class Distribution Container.

---
+++ ViewClassDistribution/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ntfish_classdistribution',
+    packages=['src'],
+    version='0.0.1',
+    description='App to determine the class distribution of fish species from JSON',
+    author='KB',
+    license='MIT',
+)
