c14a155aedf9f49188839b1ba9f86e2d08d50141
Steve Van Bodegraven <stvanbod@microsoft.com>
2019-02-27 20:29:04 +0930

    Create Metadata Container: Remove AKS Charts for the moment. Build and deploy via shell script.

--- Containers/CreateMetadata/Deploy.sh
+++ Containers/CreateMetadata/Deploy.sh
@@ -3,10 +3,7 @@ subscription="3191ba83-be2b-4b29-8409-f06e2fbb65bd"
 acr_name="InstanceSegmentationPipeline"
 az login
 az account set --subscription $subscription
-cd ./Container
-docker build -t view_class_distribution .
 acr_login_server=$(az acr show --name $acr_name --query loginServer --output tsv)
-tag="$acr_login_server/view_class_distribution"
-docker tag view_class_distribution $tag
-az acr login --name $acr_name
-docker push $tag
+tag="$acr_login_server/create_metadata"
+az acr login --name "$acr_name"
+docker run -itd -p 8890:8890 $tag
